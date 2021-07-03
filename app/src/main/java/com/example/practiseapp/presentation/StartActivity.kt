0196826df3fa1b376444c64631ef6c740ae91f62
Dmitry <darksign@list.ru>
2021-07-03 19:03:39 +0300

    [WIP] Setting up navigation and activities along with fragments Navigation component dependencies has been added 2 navigation graphs has been and placeholders for navigation destinations have been created, bottom navigation view has been added to MainActivity XML

--- app/src/main/java/com/example/practiseapp/presentation/StartActivity.kt
+++ app/src/main/java/com/example/practiseapp/presentation/StartActivity.kt
@@ -1,11 +1,12 @@
-package com.example.practiseapp
+package com.example.practiseapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.practiseapp.R
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
     }
 }
