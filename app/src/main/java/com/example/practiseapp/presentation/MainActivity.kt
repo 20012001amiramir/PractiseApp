217214d4140af09c2e272fd29c13fdac374d87ae
Dmitry <darksign@list.ru>
2021-07-05 03:35:14 +0300

    [3] BUGFIX: Duplicate classes

--- app/src/main/java/com/example/practiseapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/practiseapp/presentation/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.practiseapp.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.practiseapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
--- app/src/main/java/com/example/practiseapp/presentation/StartActivity.kt
+++ app/src/main/java/com/example/practiseapp/presentation/StartActivity.kt
@@ -3,10 +3,12 @@ package com.example.practiseapp.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.practiseapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class StartActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class StartActivity : AppCompatActivity(R.layout.activity_start) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+
     }
 }
