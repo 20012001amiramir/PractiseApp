a3fa7747d39d576c21b33816f6bba29a1197ca9a
Dmitry <darksign@list.ru>
2021-07-06 17:49:10 +0300

    [7] feature branches has been merged into develop

--- app/src/main/java/com/example/practiseapp/presentation/StartActivity.kt
+++ app/src/main/java/com/example/practiseapp/presentation/StartActivity.kt
@@ -5,12 +5,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-
+import com.welie.blessed.BluetoothCentralManager
+import com.welie.blessed.ConnectionFailedException
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
 import com.example.practiseapp.Constants
 import com.example.practiseapp.domain.entities.SessionManager
 import com.example.practiseapp.presentation.main.MainActivity
 import com.example.practiseapp.presentation.welcome.WelcomeActivity
-import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,6 +23,12 @@ class StartActivity : AppCompatActivity() {
 
     @Inject
     lateinit var sessionManager: SessionManager
+
+    private val BATTERY_SERVICE_UUID: UUID = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb")
+    private val DEVICE_SERVICE_UUID: UUID = UUID.fromString("0000180A-0000-1000-8000-00805f9b34fb")
+    private var arrayUUI = ArrayList<UUID>()
+    private lateinit var central: BluetoothCentralManager
+    val scope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,36 +40,7 @@ class StartActivity : AppCompatActivity() {
             WelcomeActivity.start(this)
             finish()
         }
-    }
 
-    companion object {
-
-        fun start(context: Context) {
-            val intent = Intent(context, StartActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context.startActivity(intent)
-
-import com.example.practiseapp.R
-import com.welie.blessed.BluetoothCentralManager
-import com.welie.blessed.BluetoothPeripheral
-import com.welie.blessed.ConnectionFailedException
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.util.*
-
-
-@AndroidEntryPoint
-class StartActivity : AppCompatActivity(R.layout.activity_main) {
-
-    private val BATTERY_SERVICE_UUID: UUID = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb")
-    private val DEVICE_SERVICE_UUID: UUID = UUID.fromString("0000180A-0000-1000-8000-00805f9b34fb")
-    private var arrayUUI = ArrayList<UUID>()
-    private lateinit var central: BluetoothCentralManager
-    val scope = CoroutineScope(Dispatchers.IO)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         central = BluetoothCentralManager(this)
         central.scanForPeripheralsWithServices(
             arrayOf(
@@ -69,14 +51,23 @@ class StartActivity : AppCompatActivity(R.layout.activity_main) {
             central.stopScan()
             scope.launch {
                 try {
-                    Log.println(Log.ASSERT,"name",peripheral.name)
-                    Log.println(Log.ASSERT,"rssi",scanResult.rssi.toString())
-                    Log.e("success","connection good!")
+                    Log.println(Log.ASSERT, "name", peripheral.name)
+                    Log.println(Log.ASSERT, "rssi", scanResult.rssi.toString())
+                    Log.e("success", "connection good!")
                 } catch (connectionFailed: ConnectionFailedException) {
-                    Log.e("error","connection failed(")
+                    Log.e("error", "connection failed(")
                 }
             }
 
+        }
+    }
+
+    companion object {
+
+        fun start(context: Context) {
+            val intent = Intent(context, StartActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(intent)
         }
     }
 }
